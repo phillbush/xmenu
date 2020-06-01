d584fba62674833135a9cb3eb9ec0f0e3fe96743
phillbush <phillbush@cock.li>
2020-06-01 11:24:24 -0300

    Updating config.mk, manual and README

--- config.mk
+++ config.mk
@@ -5,6 +5,9 @@ PROG = xmenu
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
+LOCALINC = /usr/local/include
+LOCALLIB = /usr/local/lib
+
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
@@ -13,8 +16,8 @@ FREETYPEINC = /usr/include/freetype2
 #FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
-INCS = -I/usr/local/include -I${X11INC} -I${FREETYPEINC}
-LIBS = -L/usr/local/lib -L${X11LIB} -lfontconfig -lXft -lX11 -lImlib2
+INCS = -I${LOCALINC} -I${X11INC} -I${FREETYPEINC}
+LIBS = -L${LOCALLIB} -L${X11LIB} -lfontconfig -lXft -lX11 -lImlib2
 
 # flags
 CPPFLAGS =
