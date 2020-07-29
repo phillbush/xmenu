26cb0bfcf256f68b54a4a272519a71d71c1fe13a
phillbush <phillbush@cock.li>
2020-07-29 18:09:06 -0300

    Adding config.mk

--- config.mk
+++ config.mk
@@ -17,7 +17,7 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${LOCALINC} -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${LOCALLIB} -L${X11LIB} -lfontconfig -lXft -lX11 -lImlib2
+LIBS = -L${LOCALLIB} -L${X11LIB} -lfontconfig -lXft -lX11 -lXinerama -lImlib2
 
 # flags
 CPPFLAGS =
